0f81d23b7be30165f07f4b49caa3216c40f72cab
Gregory Rompala <41299049+grrompala@users.noreply.github.com>
2021-08-15 17:20:43 -0400

    Delete twitter_token.R

--- www/twitter_token.R
+++
@@ -1,7 +0,0 @@
-twitter_token <- create_token(
-app = "shiny.tweets",
-consumer_key = "TWPAE3rLRDsSY39oURmIMz0ki",
-consumer_secret = "DlQk15mjpZ5z5GevNjDMzfeYZKwL6LGcf7NTQZN7M21uAaYuT6",
-set_renv = TRUE,
-access_token="1314548688321118209-pvACZ3t1a8JfmcDTjAM0qVF8COuDwu",
-access_secret="dYy9B4j50wKV972vQmZvI5RmHlKhsvYv9s2RS6NHea8pI")
